ab7d132cd3fa5a49149eeadd8446b5e130866bd4
Thomas Woolford <twoolie@blackhats.net.au>
2016-10-17 17:47:28 +1030

    Make admin interface more useful

--- beta/admin.py
+++ beta/admin.py
@@ -1,10 +1,20 @@
-from django.contrib import admin 
-from beta.models import BetaSignup 
+from django.conf import settings
+from django.contrib import admin
+from beta.models import BetaSignup
+
+capture_first = getattr(settings, 'BETA_CAPTURE_FIRST', False)
+capture_both  = getattr(settings, 'BETA_CAPTURE_BOTH', False)
 
 class BetaSignupAdmin(admin.ModelAdmin):
-    model = BetaSignup 
+    model = BetaSignup
+    list_display = ['email']
+    if capture_first or capture_both:
+        list_display.append('first_name')
+    if capture_both:
+        list_display.append('last_name')
+    list_display += ['contacted', 'registered']
     list_filter = ('contacted', 'registered')
-    search_fields = ('email', 'first_name', 'last_name') 
+    search_fields = ('email', '^first_name', '^last_name')
 
 admin.site.register(BetaSignup, BetaSignupAdmin)
 
